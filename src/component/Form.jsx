c975467cc3796e1a2f49956fa573ee29d45f3181
Quang <159697999+progressive-newbie263@users.noreply.github.com>
2024-09-01 15:48:17 +0700

    fix image file path via adding directly svg
    change img src into direct svg file path

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+//note: i can't do anything about svg file path, so i had to list them directly into the image.
+
 function Form() {
   const [email, setEmail] = useState("");
   const [errorVisible, setErrorVisible] = useState(false);
@@ -54,7 +56,8 @@ function Form() {
       {/* for error icon (red exclaim mark) */}
       {errorVisible && (
         <div id="icon-error" style={{ display: "block", color: "red" }}>
-          <img src="/src/images/icon-error.svg" alt="icon-error" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><g fill="none" fill-rule="evenodd"><circle cx="12" cy="12" r="12" fill="#F96464"/><path fill="#FFF" fill-rule="nonzero" d="M13.256 6v9.056h-2V6h2zm-.944 12.464c-.384 0-.699-.104-.944-.312a1.027 1.027 0 0 1-.368-.824c0-.33.125-.608.376-.832.25-.224.563-.336.936-.336.373 0 .68.112.92.336.24.224.36.501.36.832 0 .341-.117.616-.352.824-.235.208-.544.312-.928.312z"/></g></svg>
+          {/* <img src="/src/images/icon-error.svg" alt="icon-error" /> */}
           {/*
             alternate error: 
             &#x26A0; 
@@ -63,7 +66,8 @@ function Form() {
       )}
 
       <button id="button" type="submit">
-        <img src="/src/images/icon-arrow.svg" alt="icon-arrow" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 1l8.836 8.836L1 18.671"/></svg>
+        {/* <img src="/src/images/icon-arrow.svg" alt="icon-arrow" /> */}
       </button>
 
       {/* return message in green text (successful) */}
